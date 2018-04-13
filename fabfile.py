3b164a4a16f36e172a1802c051f2b1907f6239b0
Corey Oordt <coreyoordt@gmail.com>
2018-04-13 12:47:43 -0500

    Update fabfile to complete create_test_instance and remove_test_instance

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,37 @@
+import codecs
+import os
+import io
 import boto3
 from botocore.exceptions import ClientError
-from fabric.api import env, local
+from fabric.api import env, local, sudo, run
+from fabric.contrib.files import upload_template, exists
+
+escape_decoder = codecs.getdecoder('unicode_escape')
+
+if os.path.exists('.env'):
+    for line in io.open('.env'):
+        line.strip()
+        if not line or line.startswith('#') or '=' not in line:
+            continue
+        k, v = line.split('=', 1)
+        k, v = k.strip(), v.strip()
+        if v:
+            v = v.encode('unicode-escape').decode('ascii')
+            quoted = v[0] == v[-1] in ['"', "'"]
+            if quoted:
+                v = escape_decoder(v[1:-1])[0]
+        os.environ[k] = v
 
 env.app_name = 'testdemo'
+env.use_ssh_config = True
+env.hosts = ['test']
+
+
+def _authenticate_to_registry():
+    """
+    Authenticate to the ECR registry for docker
+    """
+    run('eval $(aws ecr get-login --no-include-email --region us-east-1)')
 
 
 def _repository_exists(repo_name):
@@ -23,41 +52,78 @@ def _repository_exists(repo_name):
             raise e
 
 
-def _create_repository(instance_name):
+def _create_repository():
     """
     Create a new Docker repository on ECR using the app_name/instance_name as
     the name of the repo
 
     returns the repository URL
     """
-    repo_name = '{}/{}'.format(env.app_name, instance_name)
+    import json
+
+    lifecycle_policy = {
+        "rules": [
+            {
+                "rulePriority": 1,
+                "description": "Keep only one untagged image, expire all others",
+                "selection": {
+                    "tagStatus": "untagged",
+                    "countType": "imageCountMoreThan",
+                    "countNumber": 1
+                },
+                "action": {
+                    "type": "expire"
+                }
+            }
+        ]
+    }
+    repo_name = '{app_name}/{instance_name}'.format(**env)
     existing_repo = _repository_exists(repo_name)
     if not existing_repo:
         ecr = boto3.client('ecr')
         try:
             response = ecr.create_repository(repositoryName=repo_name)
-            return response['repository']['repositoryUri']
+            url = response['repository']['repositoryUri']
+            ecr.put_lifecycle_policy(
+                repositoryName=repo_name,
+                lifecyclePolicyText=json.dumps(lifecycle_policy))
+            return url
         except ClientError as e:
             raise e
     else:
         return existing_repo['repositoryUri']
 
 
-def _build(instance_name):
+def _delete_repository():
+    """
+    Delete an existing Docker repository on ECR using the app_name/instance_name as
+    the name of the repo
+    """
+    repo_name = '{app_name}/{instance_name}'.format(**env)
+    existing_repo = _repository_exists(repo_name)
+    if existing_repo:
+        ecr = boto3.client('ecr')
+        try:
+            ecr.delete_repository(repositoryName=repo_name, force=True)
+        except ClientError as e:
+            raise e
+
+
+def _build():
     """
     Build the container
     """
     local('docker image prune -f')
-    local('./bin/docker-build -a {} -i {}'.format(env.app_name, instance_name))
+    local('./bin/docker-build -a {} -i {}'.format(env.app_name, env.instance_name))
 
 
-def _upload_to_repository(repository_url, instance_name):
+def _upload_to_repository():
     """
     Tag the docker image, upload to repository
     """
     kwargs = {
-        'project_name': '{}/{}'.format(env.app_name, instance_name),
-        'repository': repository_url,
+        'project_name': '{}/{}'.format(env.app_name, env.instance_name),
+        'repository': env.repository_url,
     }
     docker_cmd = "docker tag {project_name} {repository}:latest".format(**kwargs)
     local(docker_cmd)
@@ -68,25 +134,118 @@ def _upload_to_repository(repository_url, instance_name):
     local(docker_cmd)
 
 
+def _setup_path():
+    """
+    Set up the path on the remote server
+    """
+    path = '/testing/{}/{}'.format(env.app_name, env.instance_name)
+    sudo('mkdir -p {}'.format(path))
+
+    # Set up the permissions on all the paths
+    sudo('chgrp -R docker /testing')
+    sudo('chmod -R g+w /testing')
+
+
+def _remove_path():
+    """
+    Remove the path o the remote server
+    """
+    path = '/testing/{}/{}'.format(env.app_name, env.instance_name)
+    if exists(path):
+        sudo('rm -Rf {}'.format(path))
+
+
+def _setup_service():
+    """
+    Set up the service
+    """
+    # Create the container if it doesn't exist
+    containers = run('docker ps -a --filter name={app_name}-{instance_name} --format "{{{{.ID}}}}"'.format(**env))
+    if len(containers) == 0:
+        run('docker create --env-file /testing/{app_name}/{instance_name}/test.env --name {app_name}-{instance_name} {repository_url}:latest'.format(**env))
+
+    systemd_template = 'deploy/systemd-test.conf.template'
+    systemd_tmp_dest = '/tmp/{instance_name}.service'.format(**env)
+    systemd_dest = '/etc/systemd/system/{instance_name}.service'.format(**env)
+    if not exists(systemd_dest):
+        upload_template(systemd_template, systemd_tmp_dest, env.context)
+        sudo('mv {} {}'.format(systemd_tmp_dest, systemd_dest))
+        sudo('systemctl enable {instance_name}.service'.format(**env))
+        sudo('systemctl start {instance_name}.service'.format(**env))
+
+
+def _remove_service():
+    """
+    Stop the service, and remove its configuration
+    """
+    systemd_dest = '/etc/systemd/system/{instance_name}.service'.format(**env)
+    if exists(systemd_dest):
+        sudo('systemctl disable {instance_name}.service'.format(**env))
+        sudo('systemctl stop {instance_name}.service'.format(**env))
+        sudo('rm {}'.format(systemd_dest))
+
+
+def _setup_templates():
+    """
+    Write the templates to the appropriate places
+    """
+    env_template = 'deploy/env-test.template'
+    env_dest = '/testing/{app_name}/{instance_name}/test.env'.format(**env)
+    if not exists(env_dest):
+        upload_template(env_template, env_dest, env.context)
+
+
+def _update_image():
+    """
+    Pull down the latest version of the image from the repository
+    """
+    run("eval $(aws ecr get-login --no-include-email --region us-east-1) && "
+        "docker pull {repository_url}:latest".format(**env))
+
+
+def _prune_docker():
+    """
+    Tell docker to get rid of unused images
+    """
+    run('docker container prune -f')
+    run('docker image prune -f')
+
+
 def create_test_instance(branch_name, instance_name=''):
     """
     @brief      Create a new test instance
 
     @param      branch_name    The branch name
-    @param      instance_name  The instance name
-
+    @param      instance_name  The instance name, if different than the branch name
     """
-    # 1. Create docker repository
-    # 2. Build
-    # 3. Upload to docker repository
-    # 4. Connect to test server
-    # 5. Create test instance path
-    # 6. Pull down image
-    # 7. Create environment config with VIRTUAL_HOST
-    # 8. docker run
-    # 6. Create systemd config from template
     if instance_name == '':
         instance_name = branch_name
-    repository_url = _create_repository(instance_name)
-    # _build(instance_name)
-    _upload_to_repository(repository_url, instance_name)
+    env.instance_name = instance_name
+    env.branch_name = branch_name
+    env.release = local('git rev-parse --verify HEAD')
+    env.context = {
+        'APP_NAME': env.app_name,
+        'INSTANCE_NAME': instance_name,
+        'BRANCH_NAME': env.branch_name,
+        'RELEASE': env.release,
+    }
+    env.repository_url = _create_repository()
+    _build()
+    _upload_to_repository()
+    _setup_path()
+    _update_image()
+    _setup_templates()
+    _setup_service()
+
+
+def remove_test_instance(instance_name):
+    """
+    @brief      Removes a test instance.
+
+    @param      instance_name  The instance name
+    """
+    env.instance_name = instance_name
+    _delete_repository()
+    _remove_service()
+    _remove_path()
+    _prune_docker()
